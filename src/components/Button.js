b65c98c57aa8a9eddf2acca21dda53ca08d285fe
Sahand Omarzai <sahand.omarzai@gmail.com>
2025-10-27 22:18:36 +0100

    removed logos, changed title, fixed typo

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import './Button.css';
 
 const Button = (props) => {
     return (
-        <button onClick={props.CallApi}>
+        <button onClick={props.callApi}>
             Click to generate a joke.
         </button>
     );
